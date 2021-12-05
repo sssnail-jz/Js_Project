58a9b126de4d72a835873f683658468a0b1ecae2
thinkbookp14 <thinbookp14@temp.com>
2021-12-05 20:42:14 +0800

    将Snail博客项目移动到另一个仓库

--- BLOG/model/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/blog', {useNewUrlParser : true})
-.then(function(){
-	console.log('数据库连接成功');
-})
-.catch(function(){
-	console.log('数据库连接失败');
-}); 
--- BLOG/public/lib/ckeditor/plugins/codesnippet/lib/highlight/highlight-jack.js
+++
@@ -1,5 +0,0 @@
-// $(function () {
-// 	$("code").each(function () {
-// 		$(this).html("<ul id=\'codeblock'><li>" + $(this).html().replace(/\n/g, "\n</li><li>") + "\n</li></ul>");
-// 	});
-// });
